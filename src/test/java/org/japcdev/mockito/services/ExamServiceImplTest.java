e91f697abe3d1fd9e928b0af32ec91ead59c1da6
japc78 <japc.grafico@gmail.com>
2021-05-17 19:06:39 +0200

    Test basic without Mockito

--- src/test/java/org/japcdev/mockito/services/ExamServiceImplTest.java
+++ src/test/java/org/japcdev/mockito/services/ExamServiceImplTest.java
@@ -1,5 +1,8 @@
 package org.japcdev.mockito.services;
 
+import org.japcdev.mockito.models.Exam;
+import org.japcdev.mockito.repositories.ExamRepository;
+import org.japcdev.mockito.repositories.ExamRepositoryImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +11,13 @@ class ExamServiceImplTest {
 
     @Test
     void findExamByName() {
+        ExamRepository repository = new ExamRepositoryImpl();
+        ExamService service = new ExamServiceImpl(repository);
+
+        Exam exam = service.findExamByName("Mathematics");
+
+        assertNotNull(exam);
+        assertEquals(5L, exam.getId());
+        assertEquals("Mathematics", exam.getName());
     }
 }
